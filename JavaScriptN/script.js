f312db0e75fa2eec18ecad7b8e347a16d5400c51
BraulioLoz <brauliozc03@gmail.com>
2025-09-19 09:55:06 -0600

    Funciones arrow y clásicas de multiplicar y elevar al cuadrado

---
+++ JavaScriptN/script.js
@@ -0,0 +1,15 @@
+/* Función clásica para multiplicar */
+function multiplicarClasico(a, b) {
+    return a * b;
+}
+
+/* Función arrow para multiplicar */
+const multiplicarArrow = (a, b) => a * b;
+
+/* clásica para elevar al cuadrado */
+function cuadradoClasico(x) {
+    return x * x;
+}
+
+/* arrow para elevar al cuadrado */
+const cuadradoArrow = x => x * x;
